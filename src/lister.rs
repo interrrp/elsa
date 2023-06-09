3719928e45baee3285f7c89ed199ec9a9c61cfaa
watchYourCache <watchYourCache@gmail.com>
2023-06-09 19:16:57 +0800

    Add emoji option

--- src/lister.rs
+++ src/lister.rs
@@ -12,7 +12,39 @@ pub fn print_list_dir(args: &Args) {
         if file_name.starts_with(".") && !args.all {
             continue;
         }
+        let file_type = entry.file_type().unwrap();
 
+        if args.emoji {
+            if file_type.is_dir() {
+                print!("📁 ");
+            } else {
+                let extension = extension(&file_name).unwrap_or("");
+                print!("{} ", emoji_for(&extension));
+            }
+        }
         println!("{}", file_name);
     }
+}
+
+/// Return an emoji corresponding to the file extension.
+///
+/// This will try its best to return the most appropriate emoji for the file extension, otherwise it
+/// will return a generic file emoji (📄).
+fn emoji_for(extension: &str) -> &'static str {
+    match extension {
+        "jpg" | "jpeg" | "png" | "gif" | "webp" | "svg" | "ico" | "bmp" | "tiff" | "tif" => "🖼️",
+        "mp4" | "webm" | "mkv" | "mov" | "avi" | "wmv" | "mpg" | "mpeg" | "m4v" | "flv" => "🎞️",
+        "mp3" | "wav" | "ogg" | "flac" | "m4a" | "wma" | "aac" | "opus" => "🎵",
+        "zip" | "tar" | "gz" | "bz2" | "xz" | "7z" | "rar" | "zst" | "lzma" | "cab" | "iso"
+        | "dmg" | "pkg" | "deb" | "rpm" | "apk" => "🗜️",
+        "exe" | "msi" | "bat" | "sh" | "cmd" | "app" | "elf" | "so" | "dll" => "🖥️",
+        _ => "📄",
+    }
+}
+
+/// Return the extension of a file name.
+fn extension(file_name: &str) -> Option<&str> {
+    let mut parts = file_name.split('.');
+    let _ = parts.next();
+    parts.next()
 }
