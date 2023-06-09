b68bff4159c96e3ba55de7d1a983582d9f1ebad5
watchYourCache <watchYourCache@gmail.com>
2023-06-09 19:02:08 +0800

    Change path argument type to PathBuf

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,7 @@
 //! Contains command-line argument definitions using [Clap](https://clap.rs).
 
+use std::path::PathBuf;
+
 use clap::Parser;
 
 /// The command-line arguments.
@@ -7,7 +9,7 @@ use clap::Parser;
 pub struct Args {
     /// The path to directory to list. If not specified, the current directory is used.
     #[arg(default_value = ".")]
-    pub dir_path: String,
+    pub dir_path: PathBuf,
 
     /// Whether to show hidden files (files starting with a dot).
     #[arg(short, long)]
