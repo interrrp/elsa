d9802c8ae84b71c65ac08683b9492cf49524aaf0
watchYourCache <watchYourCache@gmail.com>
2023-06-09 19:01:16 +0800

    Pass in args directly to print_list_dir

--- src/lister.rs
+++ src/lister.rs
@@ -1,11 +1,13 @@
 //! Contains directory listing logic (see `print_list_dir`).
 
+use crate::args::Args;
+
 /// Print the contents of a directory to stdout.
-pub fn print_list_dir(dir_path: &str, show_hidden: bool) {
-    let entries = std::fs::read_dir(dir_path).unwrap();
+pub fn print_list_dir(args: &Args) {
+    let entries = std::fs::read_dir(&args.dir_path).unwrap();
     for entry in entries {
         let file_name = entry.unwrap().file_name().into_string().unwrap();
-        if file_name.starts_with(".") && !show_hidden {
+        if file_name.starts_with(".") && !args.all {
             continue;
         }
 
