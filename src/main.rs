97d8efd6c088105ba376b52e370f41b0ea4094fa
watchYourCache <watchYourCache@gmail.com>
2023-06-09 18:36:53 +0800

    Add initial directory listing implementation

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ mod args;
 fn main() {
     let args = args::Args::parse();
 
-    if &args.dir_path == "." {
-        println!("Listing the current directory");
-    } else {
-        println!("Listing directory {}", args.dir_path);
+    let entries = std::fs::read_dir(&args.dir_path).unwrap();
+    for entry in entries {
+        let file_name = entry.unwrap().file_name();
+
+        println!("{}", file_name.to_string_lossy());
     }
 }
